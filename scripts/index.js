49af219bb2748673fca5bb50753760354e2f0c06
Evgeniy Dymov <krionic1@gmail.com>
2023-04-22 09:55:58 +0300

    Исправление по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,25 @@ const popupElement = document.querySelector(".popup");
 
 const openPopupBtn = document.querySelector(".profile__edit-name");
 const closePopupBtn = popupElement.querySelector(".popup__close");
-const togglePopupVisibility = function () {
-  popupElement.classList.toggle('popup_is-opened');
-}
-openPopupBtn.addEventListener('click', togglePopupVisibility);
-closePopupBtn.addEventListener('click', togglePopupVisibility);
 
 const formElement = popupElement.querySelector(".popup__form");
-const nameInput = formElement.querySelector(".popup__name");
-const jobInput = formElement.querySelector(".popup__profession");
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const jobInput = formElement.querySelector(".popup__input_type_profession");
 
 const nameElement = document.querySelector(".profile__name");
 const professionElement = document.querySelector(".profile__profession");
 
-nameInput.value = nameElement.textContent;
-jobInput.value = professionElement.textContent;
+const openPopup = function () {
+  nameInput.value = nameElement.textContent;
+  jobInput.value = professionElement.textContent;
+  popupElement.classList.add('popup_opened');
+}
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
+}
+
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
