b8f3540b12409ef5a0b88f6389b7dd058df873bd
Evgeniy Dymov <krionic1@gmail.com>
2023-05-28 03:09:33 +0300

    Увеличивает размер свободного места для ошибки, исправляет некорректное поведение полей ввода и ошибок при повторном открытии попапов

--- scripts/index.js
+++ scripts/index.js
@@ -9,14 +9,19 @@ const popupCloseButtonElements = document.querySelectorAll(".popup__close");
 
 const formNameElement = popupNameElement.querySelector(".popup__form");
 const nameInputElement = formNameElement.querySelector(".popup__input_type_name");
+const nameErrorElement = formNameElement.querySelector(`#${nameInputElement.name}-error`)
 const jobInputElement = formNameElement.querySelector(".popup__input_type_profession");
+const jobErrorElement = formNameElement.querySelector(`#${jobInputElement.name}-error`)
 const nameElement = document.querySelector(".profile__name");
 const professionElement = document.querySelector(".profile__profession");
+const buttonNameSubmitElement = formNameElement.querySelector(".popup__submit");
 
 const formCardElement = popupCardElement.querySelector(".popup__form");
 const titleInputElement = formCardElement.querySelector(".popup__input_type_title");
+const titleErrorElement = formCardElement.querySelector(`#${titleInputElement.name}-error`)
 const linkInputElement = formCardElement.querySelector(".popup__input_type_link");
-const buttonSubmitElement = formCardElement.querySelector(".popup__submit");
+const linkErrorElement = formCardElement.querySelector(`#${linkInputElement.name}-error`)
+const buttonCardSubmitElement = formCardElement.querySelector(".popup__submit");
 
 const photoElement = popupPhotoElement.querySelector(".popup__photo");
 const photoSubtitleElement = popupPhotoElement.querySelector(".popup__subtitle");
@@ -26,22 +31,41 @@ function openPopup(popupElement) {
   window.addEventListener('keydown', closeByEsc);
 }
 
-const openNamePopup = function () {
+function resetNamePopup() {
   nameInputElement.value = nameElement.textContent;
+  nameInputElement.classList.remove('popup__input_type_error');
+  nameErrorElement.textContent = "";
+
   jobInputElement.value = professionElement.textContent;
+  jobInputElement.classList.remove('popup__input_type_error');
+  jobErrorElement.textContent = "";
+  disableButton(buttonNameSubmitElement, { inactiveButtonClass: 'popup__submit_disabled' })
+}
+
+function resetCardPopup() {
+  titleInputElement.value = "";
+  titleInputElement.classList.remove('popup__input_type_error');
+  titleErrorElement.textContent = "";
+
+  linkInputElement.value = "";
+  linkInputElement.classList.remove('popup__input_type_error');
+  linkErrorElement.textContent = "";
+  disableButton(buttonCardSubmitElement, { inactiveButtonClass: 'popup__submit_disabled' })
+}
+
+const openNamePopup = function () {
+  resetNamePopup();
   openPopup(popupNameElement)
 }
 
 const openCardPopup = function () {
-  titleInputElement.value = "";
-  linkInputElement.value = "";
+  resetCardPopup();
   openPopup(popupCardElement);
 }
 
 const closePopup = function (elem) {
   elem.classList.remove('popup_opened');
   window.removeEventListener('keydown', closeByEsc);
-  disableButton(elem.querySelector('.popup__submit'), {inactiveButtonClass: 'popup__submit_disabled'})
 }
 
 function handlePopupCloseOnClick(evt) {
@@ -100,7 +124,7 @@ function renderCard(card) {
 
 function handleCardFormSubmit(evt) {
   evt.preventDefault();
-  renderCard(createCard({name: titleInputElement.value, link: linkInputElement.value}));
+  renderCard(createCard({ name: titleInputElement.value, link: linkInputElement.value }));
   closePopup(popupCardElement);
 }
 
